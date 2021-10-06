d70437e25e21e60017217658448de50b30a7b383
uzma <ubkhan3@gmail.com>
2021-10-06 14:34:13 +0000

    complete function 1 and 2

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
 //1
-function absoluteValue(){
+function absoluteValue(num){
+    if(num > 0){
+        return num;
+    }else if(num < 0){
+        return (num * -1);
+    }else 
+        return 'null';
   
 }
+
+console.log(absoluteValue(-55));
+
+//2
+function multiplesOfFourAndSix(){
+    let arr = [];
+    for(let i=1; i<101; i++){
+        if(i%4===0 && i%6===0){
+            arr.push(i);
+        }
+    }
+    return arr;
+}
+
+console.log(multiplesOfFourAndSix());
 
 
 
